b373842f69a70c72c79e8035ca4fd45174affa1a
Tyler Sangster <tylersangster@gmail.com>
2013-07-24 20:22:30 +0000

    Say 4 - problem 23

--- say.rb
+++ say.rb
@@ -2,35 +2,38 @@ class Say
   # I used a hash instead of an array so I could map each
   # number to it`s own english word.
   ENGLISH = {
-     0 => 'zero', 1 => 'one', 2 => 'two', 3 => 'three', 4 => 'four', 5 => 'five',
-     6 => 'six', 7 => 'seven', 8 => 'eight', 9 => 'nine',
+     0 => 'zero ', 1 => 'one ', 2 => 'two ', 3 => 'three ', 4 => 'four ', 5 => 'five ',
+     6 => 'six ', 7 => 'seven ', 8 => 'eight ', 9 => 'nine ',
 
-     10 => 'ten', 11 => 'eleven', 12 => 'twelve', 13 => 'thirteen',
-     14 => 'fourteen', 15 => 'fifteen', 16 => 'sixteen',
-     17 => 'seventeen', 18 => 'eighteen', 19 => 'ninteen',
+     10 => 'ten ', 11 => 'eleven ', 12 => 'twelve ', 13 => 'thirteen ',
+     14 => 'fourteen ', 15 => 'fifteen ', 16 => 'sixteen ',
+     17 => 'seventeen ', 18 => 'eighteen ', 19 => 'ninteen ',
 
-     20 => 'twenty', 30 => 'thirty', 40 => 'forty',
-     50 => 'fifty', 60 => 'sixty', 70 => 'seventy',
-     80 => 'eighty', 90 => 'ninety',
+     20 => 'twenty ', 30 => 'thirty ', 40 => 'forty ',
+     50 => 'fifty ', 60 => 'sixty ', 70 => 'seventy ',
+     80 => 'eighty ', 90 => 'ninety ',
 
-     # 100 => 'one hundred ',     200 => 'two hundred ',
-     # 300 => 'three hundred ',   400 => 'four hundred ',
-     # 500 => 'five hundred ',    600 => 'six hundred ',
-     # 700 => 'seven hundred ',   800 => 'eight hundred ',
-     # 900 => 'nine hundred '
-  }
-  
+     100 => 'one hundred ',     200 => 'two hundred ',
+     300 => 'three hundred ',   400 => 'four hundred ',
+     500 => 'five hundred ',    600 => 'six hundred ',
+     700 => 'seven hundred ',   800 => 'eight hundred ',
+     900 => 'nine hundred ',
+     1000 => 'thousand ', 1000000 => 'million '
+  } 
+
   def initialize(num)
     @number = num
   end
 
   def in_english()
-
-    #check to see if the number is less then 0
-    #take the absolute value of a number
-    if @number < 0 || @number > 99
-    	puts "#{@number} is not within the range of  0 - 99"
-        raise ArgumentError, "Number is out of range "
+    if @number < 0 || @number > 999999999
+    	puts "#{@number} is not within the range of  0 - 99999999999"
+      raise ArgumentError, "Number is out of range "
+    elsif @number > 1000000 && @number < 1000000000
+      puts @number%1000000
+        _in_english(@number/1000000) + _in_english((@number%1000000)/1000+1000000) + _in_english(@number%1000+1000)
+    elsif @number > 1000 && @number < 1000000
+        _in_english(@number/1000) + _in_english(@number%1000+1000)
     else
     	_in_english(@number)
     end
@@ -42,14 +45,10 @@ class Say
 
   private
 	  def _in_english(number)
-	    # First all the keys from my hash that are less than or Equal to my number.
-	    # Then with the .max I take the largest of them in index
-	    # example number = 156  index=100
 	    index = ENGLISH.keys.select {|n| n <= number}.max
-
-	    # This uses recursion to break the problem into steps.
-	    # I have already found the first digit now I run the method
-	    # again to find the next digit until I have found complete answer.
-	    ENGLISH[index] + (index < number ? " " +  _in_english(number-index) : '')
-	  end
+	    ENGLISH[index] + (index < number ? _in_english(number-index) : '')
+    end
 end
+
+tyler = Say.new(273495847)
+puts tyler.in_english
--- say_test.rb
+++ say_test.rb
@@ -1,37 +1,52 @@
-require_relative './say'
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative './say'
 
 class SayTest < MiniTest::Unit::TestCase
-  
-  def test_initialize_accepts_a_number
-  	assert_not_nil Say.new(31)
-  end
-
   def test_0
     assert_equal 'zero', Say.new(0).in_english
   end
+  
+  def test_initialize_accepts_a_number
+  	assert Say.new(31)
+  end
+
+  def test_0
+    assert_equal 'zero ', Say.new(0).in_english
+  end
 
   def test_14
-    assert_equal 'fourteen', Say.new(14).in_english
+    assert_equal 'fourteen ', Say.new(14).in_english
   end
 
   def test_50
-    assert_equal 'fifty', Say.new(50).in_english
+    assert_equal 'fifty ', Say.new(50).in_english
   end
   
   def test_98
-    assert_equal 'ninety eight', Say.new(98).in_english
+    assert_equal 'ninety eight ', Say.new(98).in_english
   end
 
-  # def test_neg_1
-  #   assert_equal 'fourteen', Say.new(-1).in_english
-  # end
+  def test_1001
+    assert_equal 'one thousand one ', Say.new(1001).in_english
+  end
 
-  # def test_100
-  #   assert_equal 'fourteen', Say.new(100).in_english
-  # end
-  # def test_only_accepts_positive_numbers
-  #   assert_raise(ArgumentError){raise ArgumentError.new("Number is out of range ")} 
-  # end
+  def test_9999
+    assert_equal 'nine thousand nine hundred ninety nine ', Say.new(9999).in_english
+  end
+
+  def test_999999
+    assert_equal 'nine hundred ninety nine thousand nine hundred ninety nine ', Say.new(999999).in_english
+  end
+  
+  def test_999999999
+    assert_equal 'nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine ', Say.new(999999999).in_english
+  end
+  
+  def test_range
+    tyler = Say.new("-1")
+    assert_raises ArgumentError do
+      tyler.in_english
+    end
+  end
 end
